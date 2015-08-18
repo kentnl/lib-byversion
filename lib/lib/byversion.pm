ada010647e218098e0935ca987fc35662682390d
Kent Fredric <kentfredric@gmail.com>
2015-08-18 16:36:08 +1200

    Build results of 3cd6483 (on master)

--- lib/lib/byversion.pm
+++ lib/lib/byversion.pm
@@ -4,7 +4,7 @@ use warnings;
 
 package lib::byversion;
 
-our $VERSION = '0.002001';
+our $VERSION = '0.002002';
 
 # ABSTRACT: add paths to @INC depending on which version of Perl is running.
 
@@ -58,7 +58,7 @@ lib::byversion - add paths to @INC depending on which version of Perl is running
 
 =head1 VERSION
 
-version 0.002001
+version 0.002002
 
 =head1 SYNOPSIS
 
