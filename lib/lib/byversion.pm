00fb43720a3348c58d30302347c8a7cf27f61f59
Kent Fredric <kentfredric@gmail.com>
2015-08-18 16:04:55 +1200

    Build results of c26bf0c (on master)

--- lib/lib/byversion.pm
+++ lib/lib/byversion.pm
@@ -4,7 +4,7 @@ use warnings;
 
 package lib::byversion;
 
-our $VERSION = '0.002000';
+our $VERSION = '0.002001';
 
 # ABSTRACT: add paths to @INC depending on which version of Perl is running.
 
@@ -58,7 +58,7 @@ lib::byversion - add paths to @INC depending on which version of Perl is running
 
 =head1 VERSION
 
-version 0.002000
+version 0.002001
 
 =head1 SYNOPSIS
 
