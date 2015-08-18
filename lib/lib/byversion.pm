ad03c04fb9ea83e35aa5008342506e8a20366d05
Kent Fredric <kentfredric@gmail.com>
2015-08-18 13:33:07 +1200

    Squeeze/appease critic

--- lib/lib/byversion.pm
+++ lib/lib/byversion.pm
@@ -1,3 +1,4 @@
+use 5.006;
 use strict;
 use warnings;
 
@@ -5,7 +6,9 @@ package lib::byversion;
 
 our $VERSION = '0.002000';
 
-# ABSTRACT: add paths to @INC depending on which version of C<perl> is running.
+# ABSTRACT: add paths to @INC depending on which version of Perl is running.
+
+# AUTHORITY
 
 use lib ();
 use version 0.77;
@@ -16,14 +19,14 @@ use String::Formatter stringf => {
     v => "$]",
     V => do {
       my $x = version->parse("$]")->normal;
-      $x =~ s{^v}{};
+      $x =~ s{^v}{}sx;
       $x;
     },
-  }
+  },
 };
 
 sub import {
-  my ( $self, @args ) = @_;
+  my ( undef, @args ) = @_;
   if ( @args != 1 ) {
     die 'lib::byversion->import takes exactly one argument, instead, you specified ' . scalar @args;
   }
@@ -33,9 +36,9 @@ sub import {
 
 ## no critic (ProhibitBuiltinHomonyms)
 sub unimport {
-  my ( $self, @args ) = @_;
+  my ( undef, @args ) = @_;
   if ( @args != 1 ) {
-    die "lib::byversion->import takes exactly one argument, instead, you specified " . scalar @args;
+    die 'lib::byversion->unimport takes exactly one argument, instead, you specified ' . scalar @args;
   }
   my $path = path_format(@args);
   return lib->unimport($path);
@@ -48,14 +51,16 @@ sub unimport {
 So you have >1 Perl Installs.  You have >1 Perl installs right?
 And you switch between running them how?
 
-Let me guess, somewhere you have code that sets a different value for PERL5LIB depending on what Perl you're using.
+Let me guess, somewhere you have code that sets a different value for PERL5LIB
+depending on what Perl you're using.
 Oh you use L<< C<perlbrew>?|http://grep.cpan.me/?q=PERL5LIB+dist=App-perlbrew >>
 
 This is a slightly different approach:
 
 =over 4
 
-=item 1. Set up your user-land PERL5LIB directories in a regular pattern differing only by C<perl> version
+=item 1. Set up your user-land PERL5LIB directories in a regular pattern
+differing only by C<perl> version
 
     $HOME/Foo/Bar/5.16.0/lib/...
     $HOME/Foo/Bar/5.16.1/lib/...
@@ -71,13 +76,23 @@ The right PERL5LIB gets loaded based on which C<perl> you use.
 
 =back
 
-Yes, yes, catch 22, C<lib::byversion> and its dependencies need to be in your lib to start with.
+Yes, yes, catch 22, C<lib::byversion> and its dependencies need to be in
+your lib to start with.
 
-O.k. That is a problem, slightly. But assuming you can get that in each C<perl> install somehow, you can load each C<perl>'s user library directories magically with this module once its loaded.
+O.k. That is a problem, slightly. But assuming you can get that in each
+C<perl> install somehow, you can load each C<perl>'s user library directories
+magically with this module once its loaded.
 
-And "assuming you can get that in each C<perl> install somehow" =~ with a bit of luck, this feature or something like it might just be added to Perl itself, as this is just a prototype idea to prove it works ( or as the case may be, not ).
+And "assuming you can get that in each C<perl> install somehow" =~ with a bit
+of luck, this feature or something like it might just be added to Perl itself,
+as this is just a prototype idea to prove it works ( or as the case may be,
+not ).
 
-And even if that never happens, and you like this module, you can still install this module into all your C<perl>'s and keep a separate C<user-PERL5LIB-per-perl> without having to use lots of scripts to hold it together, and for System Perls, you may even be fortunate enough to get this module shipped by your C<OS> of choice. Wouldn't that be dandy.
+And even if that never happens, and you like this module, you can still
+install this module into all your C<perl>'s and keep a separate
+C<user-PERL5LIB-per-perl> without having to use lots of scripts to hold it
+together, and for System Perls, you may even be fortunate enough to get this
+module shipped by your C<OS> of choice. Wouldn't that be dandy.
 
 =head1 SYNOPSIS
 
@@ -96,7 +111,8 @@ or alternatively
 etc.
 
 
-C<lib::byversion> expects one parameter, a string path containing templated variables for versions.
+C<lib::byversion> expects one parameter, a string path containing templated
+variables for versions.
 
 Current defined parameters include:
 
@@ -108,9 +124,11 @@ This is an analogue of C<$^V> except :
 
 =over 4
 
-=item it should work on even C<perl>s that didn't have C<$^V>, as it converts it from C<$]> with L<version.pm|version>
+=item it should work on even C<perl>s that didn't have C<$^V>, as it converts
+it from C<$]> with L<version.pm|version>
 
-=item it lacks the preceding C<v>, because this is more usually what you want and its easier to template it in than take it out.
+=item it lacks the preceding C<v>, because this is more usually what you want
+and its easier to template it in than take it out.
 
 =back
 
@@ -128,6 +146,8 @@ Example:
 
 =back
 
-More may be slated at some future time, e.g.: to allow support for components based on C<git> C<sha1>'s, but I figured to upload something that works before I bloat it out with features nobody will ever use.
+More may be slated at some future time, e.g.: to allow support for components
+based on C<git> C<sha1>'s, but I figured to upload something that works before
+I bloat it out with features nobody will ever use.
 
 =cut
